01c119a27db92b8eb60fa6a4ab1b8e669eefaf79
Taco <130668932+54thTaco@users.noreply.github.com>
2023-06-07 22:34:31 +0800

    Delete rfo-install.sh

--- rfo-install.sh
+++
@@ -1,6 +0,0 @@
-cd /Applications/Roblox.app/Contents/MacOS
-echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://raw.githubusercontent.com/54thTaco/RFO-macOS/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
-chmod +x RobloxPlayer
-echo "RFO has been installed."
-fi
