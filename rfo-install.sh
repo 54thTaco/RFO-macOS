38f42bfb56f3b16c75326fc86db12ecd55351e16
Taco <130668932+54thTaco@users.noreply.github.com>
2023-06-07 22:08:58 +0800

    Update rfo-install.sh

--- rfo-install.sh
+++ rfo-install.sh
@@ -2,7 +2,7 @@ cd /Applications/Roblox.app/Contents/MacOS
 if [ ! -f RCOPATCHED ]; then
 mv RobloxPlayer ActualRobloxPlayer
 echo "#!/bin/sh" >> RobloxPlayer
-echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://roblox-client-optimizer.simulhost.com/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
+echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://raw.githubusercontent.com/54thTaco/RFO-macOS/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
 touch RCOPATCHED
 echo "RFO has been installed."
