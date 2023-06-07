2f4de7916bb7628ebfa74d925de674fed0ed3690
Taco <130668932+54thTaco@users.noreply.github.com>
2023-06-07 23:00:52 +0800

    Update RFO-installer.sh

--- RFO-installer.sh
+++ RFO-installer.sh
@@ -1,5 +1,4 @@
 cd /Applications/Roblox.app/Contents/MacOS
-#!/bin/sh" >> RobloxPlayer
 rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings
 mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings
 curl https://raw.githubusercontent.com/54thTaco/RFO-macOS/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json
