7c76b1b17ff6514a6d345b7cc0952c3313995ab2
Taco <130668932+54thTaco@users.noreply.github.com>
2023-06-07 22:53:36 +0800

    Add files via upload

---
+++ RFO-installer.sh
@@ -0,0 +1,6 @@
+cd /Applications/Roblox.app/Contents/MacOS
+#!/bin/sh" >> RobloxPlayer
+rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings
+mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings
+curl https://raw.githubusercontent.com/54thTaco/RFO-macOS/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json
+echo "RFO has been installed."
