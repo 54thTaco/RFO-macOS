893af5f205a8b0db7681f5d0076dd41f79cf17b8
Taco <130668932+54thTaco@users.noreply.github.com>
2023-06-09 16:56:42 +0800

    RFO-installer.sh v2 Prerelease

--- RFO-installer.sh
+++ RFO-installer.sh
@@ -1,5 +1,12 @@
 cd /Applications/Roblox.app/Contents/MacOS
 rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings
 mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings
-curl https://raw.githubusercontent.com/54thTaco/RFO-macOS/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json
-echo "RFO has been installed."
+cd /Applications/Roblox.app/Contents/MacOS/ClientSettings/
+curl https://raw.githubusercontent.com/rbxflags/Flags/main/Main/Base.json -o Base.json && curl https://raw.githubusercontent.com/rbxflags/Flags/main/Main/GraphicsBars.json -o GraphicsBars.json && curl https://raw.githubusercontent.com/rbxflags/Flags/main/Main/Privacy.json -o Privacy.json
+cat Base.json GraphicsBars.json Privacy.json >> ClientAppSettings.json
+rm -rf Base.json GraphicsBars.json Privacy.json
+sed '4d;5d;8d' ClientAppSettings.json > CAS.json
+sed '3s/$/,/' CAS.json > CAS1.json
+sed '5s/$/,/' CAS1.json > ClientAppSettings.json
+rm -rf CAS.json CAS1.json
+echo "Script finished."
